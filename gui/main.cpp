0be4372ca0eaad813a442db63accc775b5d03850
Sebastian Weiss <sebastian13.weiss@tum.de>
2022-03-09 13:45:53 +0100

    One user reported, that ImGui is not found in the GUI. Maybe this fixes it

--- gui/main.cpp
+++ gui/main.cpp
@@ -1,8 +1,8 @@
 #include <iostream>
-#include "imgui.h"
-#include "imgui_impl_glfw.h"
-#include "imgui_impl_opengl3.h"
-#include "IconsFontAwesome5.h"
+#include <imgui.h>
+#include <imgui_impl_glfw.h>
+#include <imgui_impl_opengl3.h>
+#include <IconsFontAwesome5.h>
 
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
